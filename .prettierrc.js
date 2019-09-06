9c51de6dccfc16854b792b136c3a1318f1645c23
rodrigolive <rodrigo@clarive.com>
2019-09-06 17:47:16 +0200

    linting setup

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+    parser: 'babylon',
+    printWidth: 80,
+    tabWidth: 4,
+    bracketSpacing: true,
+    singleQuote: true,
+    trailingComma: 'none',
+    arrowParens: 'avoid'
+};
